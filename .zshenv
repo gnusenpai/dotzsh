8e999fd8d881e93052a22bc4dca3704558396c5f
gnusenpai <pspslimboy98@gmail.com>
2024-05-18 23:42:14 -0400

    Move PATH to my config

--- .zshenv
+++ .zshenv
@@ -8,9 +8,6 @@ fi
 export VISUAL=$EDITOR
 export PAGER=less
 
-typeset -U path
-path=("$HOME/bin" "$HOME/.local/bin" "$HOME/.cargo/bin" $path)
-
 # Source custom configuration in env.d
 for env in "$ZDOTDIR/env.d/"*.zsh(N); do
     source $env
--- env.d/gnusenpai.zsh
+++ env.d/gnusenpai.zsh
@@ -3,3 +3,6 @@ if command -v gpgconf >/dev/null; then
 fi
 export FZF_DEFAULT_OPTS="--layout=reverse --height=33% --color=16"
 export WINEDLLOVERRIDES=winemenubuilder.exe=d
+
+typeset -U path
+path=("$HOME/bin" "$HOME/.local/bin" "$HOME/.cargo/bin" $path)
